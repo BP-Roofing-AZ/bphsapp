3bf96430a4971421090ee6e37fcb408f6f440d81
Daniel Barkley <dbarkley@blueprinthomeservice.com>
2026-07-13 09:57:40 -0700

    v5.125 — add netlify functions scaffold

---
+++ netlify/functions/support-request.js
@@ -0,0 +1,8 @@
+// Netlify function: support-request
+// Module scaffolding — implementation lives in the build.
+(function () {
+  'use strict';
+  // TODO: initialize
+})();
+
+// touched by v5.125
